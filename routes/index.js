a9c5d7635e1a884051f03d471793ae8a0fc15bc8
Dan Neame <danneame@gmail.com>
2013-09-08 20:58:03 +0100

    User dao and api methods

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@
 
 var express = require('express'),
     url = require('url'),
-    // api = require('./lib/api'),
+    api = require('../lib/api'),
     args = require('argsparser').parse();
 
 module.exports = function routing(){
@@ -16,16 +16,64 @@ module.exports = function routing(){
         next();
     }
 
-    app.get('/users', checkAuth, function(req, res){
-        // api.projects(req, res);
-        res.send({
-            username: 'cham'
+    app.get('/users', checkAuth, function(req, res, next){
+        api.users.listUsers({},function(err, data){
+            if(err){
+                return next(err);
+            }
+            res.send(data);
         });
     });
 
-    app.get('*', function(req, res){
+    app.get('/users/summary', checkAuth, function(req, res, next){
+        api.users.listUsers({summary: true}, function(err, data){
+            if(err){
+                return next(err);
+            }
+            res.send(data);
+        });
+    });
+
+    app.get('/user/:userName', checkAuth, function(req, res, next){
+        api.users.getUser({
+            username: req.route.params.userName
+        }, function(err, data){
+            if(err){
+                return next(err);
+            }
+            res.send(data);
+        });
+    });
+
+    app.get('/user/:userName/summary', checkAuth, function(req, res, next){
+        api.users.getUser({
+            username: req.route.params.userName,
+            summary: true
+        }, function(err, data){
+            if(err){
+                return next(err);
+            }
+            res.send(data);
+        });
+    });
+
+    app.get('/fakeuser', checkAuth, function(req, res, next){
+        api.users.addUser({
+            username: 'cham',
+            password: 'b33d065',
+            email: 'danneame@gmail.com',
+            ip: req.connection.remoteAddress
+        }, function(err, data){
+            if(err){
+                return next(err);
+            }
+            res.send(data);
+        });
+    });
+
+    app.get('*', function(req, res, next){
         res.statusCode = 404;
-        res.end();
+        return next();
     });
 
     return app.middleware;
